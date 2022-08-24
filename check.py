2888fc4408eb1bc09af101966c2dbe4c65b37a2c
Jason Wang <91506829+Leek727@users.noreply.github.com>
2022-08-24 18:52:27 -0400

    Add files via upload

--- check.py
+++ check.py
@@ -10,30 +10,35 @@ model = load_model('catdream.h5')
 aaa = load_model('encoder.h5')
 model.summary()
 
-a = [0,0,0,0]
+a = [0]*50
 f = 0
 for root, dirs, files in os.walk("cats", topdown=False):
     for name in files:
         path = os.path.join(root, name)
-        if ".png" in path:
-            img = np.array(Image.open(path).convert('L').getdata(), dtype=np.uint8)
-            img = img.reshape(64,64) / 255
+        
+        #img = np.array(Image.open(path).convert('L').getdata(), dtype=np.uint8)
+        #img = img.reshape(64,64) / 255
+        img = np.array(Image.open(path))
+        ff = img
             #print(img[0])
 
-            latent = aaa.predict(np.array([img]))[0]
-            for i in range(len(a)):
-                if a[i] < latent[i]:
-                    a[i] = latent[i]
+        latent = aaa.predict(np.array([img]))[0]
+        #for i in range(len(a)):
+        #    a[i] += latent[i]
 
-            print(a)
-            img = model.predict(np.array([latent]))[0]
+        print(latent)
+        img = model.predict(np.array([latent]))[0]
+        print(img[0])
            
-            # resize image
-            img = cv2.resize(img, (300,300))
-            cv2.imshow("", img)
-            cv2.waitKey(50)
+        # resize image
+        img = cv2.resize(img, (300,300))
+        cv2.imshow("", img)
+        #cv2.imshow("",ff)
+        cv2.waitKey(50)
 
-        if f > 1000:
+        if f > 100:
             break
 
         f+= 1
+
+print([x/100 for x in a])
